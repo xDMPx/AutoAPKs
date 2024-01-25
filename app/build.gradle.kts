0e719a7e59844e938c52cc2be13167385aea4437
xDMPx <57721731+xDMPx@users.noreply.github.com>
2024-01-25 13:35:06 +0100

    Set room schema location using the Room Gradle Plugin to allow for auto migrations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.devtools.ksp")
+    id("androidx.room")
 }
 
 android {
@@ -47,6 +48,10 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+}
+
+room {
+    schemaDirectory("$projectDir/schemas")
 }
 
 dependencies {
